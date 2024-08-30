dacde654ae1a55c676349c113538a3eb8873322a
Israel R. <israel.rexc@yahoo.com>
2024-08-30 15:04:51 -0300

    Update comments on ex5

--- ex5/script.js
+++ ex5/script.js
@@ -1,9 +1,12 @@
 const strInput = "Inverter os caracteres dessa string de entrada";
 
 function reverse(strInput) {
+  // Gera array a partir da string
   const arr = strInput.split("");
   const reversed = [];
+
   arr.forEach((el) => {
+    // Insere caracteres ao final do vetor
     reversed.unshift(el);
   });
   return reversed.join("");
